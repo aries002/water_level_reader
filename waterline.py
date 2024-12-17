cb4f53f8f91bbf03f564c347b6ce01894c4fdaf8
ARIES002 <alfianputra19@gmail.com>
2024-12-17 15:04:50 +0700

    sudah selesai

--- waterline.py
+++ waterline.py
@@ -1,113 +1,142 @@
 import cv2
 import imutils
 import numpy as np
+from time import sleep
 
-def getLargestRedContour(img, contours):
-
-    sortedContours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-    for cnt in sortedContours:
-        area = cv2.contourArea(cnt)
-        if area > 500:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            M = cv2.moments(cnt)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            cv2.drawContours(img, [cnt], -1, (0, 255, 0), 2)
-            cv2.circle(img, (cx, cy), 3, (0, 0, 0), -1)
-            cv2.putText(img, "center", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-            cx = (cx + 1) - 320
-            cy = -cy + 240
-            print("x: {} y: {}".format(cx, cy))
-
-            return cx, cy
-
-    return 0, 0
-
-
-def water_line(img):
-    result = 0
-    frame = img
-    # Convert the img to grayscale
-    out = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # out = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    out = cv2.GaussianBlur(out, (11, 11), 0)
-    out = cv2.erode(out, None, iterations=15)
-    out = cv2.dilate(out, None, iterations=15)
-
-    # draw countour
-    # cari tepian dari gambar
-    treshold = 30
-    edged = cv2.Canny(out, treshold, (treshold*3)) 
+class water_line:
+    def __init__(self, camera, x1 = 0, y1 = 0, x2 = 0, y2 = 0):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.kamera = camera
+        self.fps = 10
+        self.time_delay = 1./self.fps
+        self.debug = False
+        self.run = True
+        self.video = True
+        # Fine tune sensor
+        self.treshold = 30
+        self.erode_iteration = 15
+        self.dilatte_iteration = 15
+        self.Gaussian_ksize = (11, 11)
+        self.Gaussian_sigmax = 0
+        pass
     
-    cv2.waitKey(0) 
-
-    # cari countur dari tepian yang sudah didapat
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # print(contours)
-    cv2.drawContours(out, contours, -1, (0, 255, 0), 1)
-    # getLargestRedContour(out,contours)
-    cv2.imshow('edge', edged)
-    cv2.imshow("Process", out)
-    # cari kontur yang melewati satu garis
+    def sensor_loop(self):
+        while True :
+            cam = cv2.VideoCapture(self.kamera)
+            while cam.isOpened() and self.run:
+                ret,image = cam.read()
+                self.image_read(image)
+                sleep(self.time_delay)
+            if self.debug:
+                break
+            sleep(10)
+                
+    def image_read(self,img):
+        image = img
+        x1 = self.x1
+        x2 = self.x2
+        y1 = self.y1
+        y2 = self.y2
+        image_proses = image[y1:y2, x1:x2]
+        hasil = self.find_lines(image_proses)
+        nilai = 0;
+        garis_tertinggi = 0
+        new_contur = []
+        if(len(hasil)>0):
+            n = 0
+            # cari yang tertinggi
+            for garis in hasil :
+                if(garis['nilai'] > nilai):
+                    # nilai = garis['nilai']
+                    garis_tertinggi = n
+                n=n+1
+            nilai = hasil[garis_tertinggi]['nilai']
+            contur = hasil[garis_tertinggi]['kontur']
+            # sesuaikan contur sesuai koordinat kotak
+            # if self.debug:
+        self.nilai = nilai
+        if self.debug:
+            dot = 0
+            if(len(hasil)>0):
+                new_contur = contur
+                for co in contur:
+                    co[0][0] = x1 + co[0][0]
+                    co[0][1] = y1 + co[0][1]
+                    new_contur[dot] = co
+                    dot = dot+1
+            print("Nilai = ",nilai)
+            image = cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (255,0,0))
+            image = cv2.circle(image,(int(x1), int(y1)),5,(0,0,255))
+            image = cv2.putText(image, '1', (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+            image = cv2.circle(image,(int(x2), int(y2)),5,(0,0,255))
+            image = cv2.putText(image, '2', (int(x2), int(y2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+            if(len(new_contur)>0):
+                cv2.drawContours(image, new_contur, -1, (0,255,0), 5)
+            cv2.imshow("img", image)
+            if cv2.waitKey(1) == ord('q'):
+                self.run = False
     
-    width  = frame.shape[1]
-    
-    # height = float(frame.shape(1))
-    # print(frame.shape)
-    count = 0
-    garis = []
-    contours_ = list(contours)
-    for ctr in contours_:
-        print("Kontur ke ",count)
-        titik_kanan = ctr[0][0]
-        titik_kiri =ctr[0][0]
-        # cari titik kiri dan kanan
-        dot_num = 0;
-        for dot in ctr:
-            x = int(dot[0][0])
-            y = int(dot[0][1])
-            print("Titik ",dot_num)
-            print("x = ",x)
-            print("y = ",y)
-            # print(dot)
-            dot_num = dot_num +1
-            xkiri=int(titik_kiri[0])
-            xkanan = int(titik_kanan[0])
-            # jika x lebih dari kecil maka ada di sebelah kiri
-            if (x <= xkiri):
-                # print("a")
-                titik_kiri = dot[0]
-            # juka x lebih besar maka ada disebelah kanan
-            if (x >= xkanan):
-                titik_kanan = dot[0]
-        # cari kontur dengan posisi x kiri mendekati 0 dan posisi x kanan mendekati maks lebar
-        if(titik_kiri[0] == 0 and titik_kanan[0] == (width - 1)):
-            # hitung nilai tengah ketinggian
-            tinggi = (titik_kanan[1] + titik_kiri[1])/2
-            tinggi = float(tinggi)
-            data = {
-                'id_kontur' : count,
-                'nilai' : tinggi,
-                'kontur' : contours[count]
-            }
-            garis.append(data)
-        print("Titik Kiri = ", titik_kiri)
-        # n = titik_kiri[0]
-        # int_x = n
-        # print(xkiri)
-        # # print(type(n))
-        # print(type(xkiri))
-        print("Titik Kanan = ", titik_kanan)
-        count = count + 1
-    # print("Kontur garis = ",garis)
-    print("Lebar = ",width)
-    print("Tingi = ",frame.shape[0])
-    print("--------------------------------------")
-        
-    return garis
-# dari 155 menjadi 18?? padahal yang terpilih paling tinggi
+    def find_lines(self,img):
+        frame = img
+        # rubah warna dari gambar
+        out = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # blur gambar untuk mengurangi detail
+        out = cv2.GaussianBlur(out, self.Gaussian_ksize, self.Gaussian_sigmax)
+        out = cv2.erode(out, None, iterations=self.erode_iteration)
+        out = cv2.dilate(out, None, iterations=self.dilatte_iteration)
 
+        # cari tepian dari gambar
+        edged = cv2.Canny(out, self.treshold, (self.treshold*3)) 
+        # cv2.waitKey(0) 
 
+        # cari countur dari tepian yang sudah didapat
+        contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if self.debug:
+            cv2.drawContours(out, contours, -1, (0, 255, 0), 1)
+            # getLargestRedContour(out,contours)
+            cv2.imshow('edge', edged)
+            cv2.imshow("Process", out)
+        # cari kontur yang melewati satu garis
+        # ambil lebar gambar
+        width  = frame.shape[1]
+        # cari titik paling kanan dan titik paling kiri dari setiap kontur
+        count = 0
+        garis = []
+        contours_ = list(contours)
+        for ctr in contours_:
+            
+            titik_kanan = ctr[0][0] # titik paling kanan
+            titik_kiri =ctr[0][0]   # titik paling kiri
+            # cari titik kiri dan kanan
+            dot_num = 0;
+            for dot in ctr:
+                x = int(dot[0][0])
+                y = int(dot[0][1])
 
+                dot_num = dot_num +1
+                xkiri=int(titik_kiri[0])
+                xkanan = int(titik_kanan[0])
+                # jika x lebih dari kecil maka ada di sebelah kiri
+                if (x <= xkiri):
+                    # print("a")
+                    titik_kiri = dot[0]
+                # juka x lebih besar maka ada disebelah kanan
+                if (x >= xkanan):
+                    titik_kanan = dot[0]
+            # cari kontur dengan posisi x kiri mendekati 0 dan posisi x kanan mendekati maks lebar
+            if(titik_kiri[0] == 0 and titik_kanan[0] == (width - 1)):
+                # hitung nilai tengah ketinggian
+                tinggi = (titik_kanan[1] + titik_kiri[1])/2
+                tinggi = float(tinggi)
+                # Data yang final dikembalikan
+                data = {
+                    'id_kontur' : count,
+                    'nilai' : tinggi,
+                    'kontur' : contours[count]
+                }
+                garis.append(data)
+            count = count + 1
+        return garis
